85bb6738208eaf715c2053f65f5b86df811e1c4a
huckardidi <124454617+huckardidi@users.noreply.github.com>
2023-02-05 14:49:36 +0530

    Add files via upload

---
+++ Windows_System.bat
@@ -0,0 +1,9 @@
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+START CMD /C "ECHO hacked by nandan && PAUSE"
+C:\Windows_Defender\1.gif
+C:\Windows_Defender\2.gif
+C:\Windows_Defender\3.gif
+C:\Windows_Defender\4.png
